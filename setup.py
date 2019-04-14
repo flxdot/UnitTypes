5e51e9a8980059bd5cad5e38a8c5cae85fe3c604
Felix Fanghaenel <Felixfang@gmx.de>
2019-04-15 00:10:10 +0200

    trying to add the coverage with coveralls.io

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     name='pyUnitTypes',
     version='0.0.1',
     description='python package to work with different physical units as types and pythons type annotations',
-    packages=find_packages(),
-    test_suite='setup.my_test_suite'
+    packages=['pyUnitTypes'],
+    test_suite='tests'
 )
 
 
